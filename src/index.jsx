ec98c752409e5ea53ac09431dcf367bc3d6e338d
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-10 20:06:07 -0700

    update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,5 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
-const root = document.getElementById("root");
+const root = reactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
