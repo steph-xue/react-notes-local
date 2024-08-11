48fb759cc107439a55f46274caceace8916806bb
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-10 20:09:29 -0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,48 @@ import { nanoid } from "nanoid"
 function App() {
     const [notes, setNotes] = React.useState(
         () => JSON.parse(localStorage.getItem("notes")) || []
-    )
+    );
+
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0]?.id) || ""
-    )
+    );
     
     const currentNote = 
         notes.find(note => note.id === currentNoteId) 
-        || notes[0]
+        || notes[0];
 
     React.useEffect(() => {
         localStorage.setItem("notes", JSON.stringify(notes))
-    }, [notes])
+    }, [notes]);
 
     function createNewNote() {
         const newNote = {
             id: nanoid(),
             body: "# Type your markdown note's title here"
-        }
-        setNotes(prevNotes => [newNote, ...prevNotes])
-        setCurrentNoteId(newNote.id)
+        };
+        setNotes(prevNotes => [newNote, ...prevNotes]);
+        setCurrentNoteId(newNote.id);
     }
 
     function updateNote(text) {
         setNotes(oldNotes => {
-            const newArray = []
+            const newArray = [];
             for (let i = 0; i < oldNotes.length; i++) {
-                const oldNote = oldNotes[i]
+                const oldNote = oldNotes[i];
                 if (oldNote.id === currentNoteId) {
                     // Put the most recently-modified note at the top
-                    newArray.unshift({ ...oldNote, body: text })
+                    newArray.unshift({ ...oldNote, body: text });
                 } else {
-                    newArray.push(oldNote)
+                    newArray.push(oldNote);
                 }
             }
-            return newArray
-        })
+            return newArray;
+        });
     }
 
     function deleteNote(event, noteId) {
-        event.stopPropagation()
-        setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+        event.stopPropagation();
+        setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId));
     }
 
     return (
@@ -89,7 +90,7 @@ function App() {
 
             }
         </main>
-    )
+    );
 }
 
 export default App;
